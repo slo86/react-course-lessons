96074c1e85c9111d0b76c0dac07ba7039ada2eec
Oleg Slepov <slepov.ol.n@gmail.com>
2024-08-13 15:11:00 +0300

    Refactoring...

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
 export const metadata = {
-    title: 'Vite + React',
-    description: 'Vite + React description',
+    title: 'Next + React',
+    description: 'Next + React description',
 };
 
 export default function RootLayout({children}) {
     return (
         <html lang="en">
-            <head>
-                <title>Vite + React</title>
-            </head>
             <body>
                 <div id="root">{children}</div>
             </body>
